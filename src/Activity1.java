dd105bc0e44bf5581c66e8383a4d07c14a9c9e86
swinarskiJ <111909545+swinarskiJ@users.noreply.github.com>
2026-03-02 20:10:54 -0500

    sort1

--- src/Activity1.java
+++ src/Activity1.java
@@ -1,5 +1,49 @@
-public class Activity1 {
+public class Activity1 extends IntegerManager implements PrintPretty{
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Activity1 thingie = new Activity1();
+        thingie.buildList();
+        thingie.printPretty();
+    }
+
+    @Override
+    void buildList() {
+        nums = new int[20];
+        for(int i = 0; i < nums.length; i++){
+            nums[i] = (int)(Math.random()*1000) - 100;
+        }
+    }
+
+    public void printPretty(){
+        System.out.println("/n ===== ARRAY ===== (size: " + nums.length + ")");
+        for(int num: nums){
+            System.out.print(num + " ");
+        }
+    }
+
+    @Override
+    void shuffle() {
+        //loop thorugh all numbers , index i
+            //pick random spot, r
+            // 3 part sawp, r>temp>i
+    }
+
+    @Override
+    void insertionSort() {
+       
+    }
+
+    @Override
+    void selectionSort() {
+
+    }
+
+    @Override
+    void mergeSort() {
+       
+    }
+
+    @Override
+    int pickRandom() {
+        throw new UnsupportedOperationException("Unimplemented method 'pickRandom'");
     }
 }
