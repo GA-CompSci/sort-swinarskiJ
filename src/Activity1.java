8f330c51685b83a7edb358eb5620893c2825e279
swinarskiJ <111909545+swinarskiJ@users.noreply.github.com>
2026-03-03 09:17:45 -0500

    Sort2

--- src/Activity1.java
+++ src/Activity1.java
@@ -3,18 +3,20 @@ public class Activity1 extends IntegerManager implements PrintPretty{
         Activity1 thingie = new Activity1();
         thingie.buildList();
         thingie.printPretty();
+        thingie.shuffle();
+        thingie.selectionSort();
     }
 
     @Override
     void buildList() {
         nums = new int[20];
         for(int i = 0; i < nums.length; i++){
-            nums[i] = (int)(Math.random()*1000) - 100;
+            nums[i] = (int)(Math.random()*100);
         }
     }
 
     public void printPretty(){
-        System.out.println("/n ===== ARRAY ===== (size: " + nums.length + ")");
+        System.out.println("\n ===== ARRAY ===== (size: " + nums.length + ")");
         for(int num: nums){
             System.out.print(num + " ");
         }
@@ -22,9 +24,22 @@ public class Activity1 extends IntegerManager implements PrintPretty{
 
     @Override
     void shuffle() {
+       
         //loop thorugh all numbers , index i
+        for(int i = 0; i < nums.length; i++){
             //pick random spot, r
+            int r = (int)(Math.random()*nums.length);
             // 3 part sawp, r>temp>i
+            int temp = nums[i];
+            nums[i] = nums[r];
+            nums[r] = temp;
+        }
+
+        System.out.println("\n ===== SHUFFLED ARRAY ===== (size: " + nums.length + ")");
+        for(int num: nums){
+            System.out.print(num + " ");
+        }
+            
     }
 
     @Override
@@ -34,7 +49,26 @@ public class Activity1 extends IntegerManager implements PrintPretty{
 
     @Override
     void selectionSort() {
+        int i,j,temp;
+        for (i = 0; i < nums.length; i++){
+            int smallest = i;
 
+            for(j = i+1; j <nums.length; j++){
+                if(nums[j]<nums[smallest]){
+                    smallest = j;
+                    temp = nums[j];
+                    nums[j] = nums[i];
+                    nums[i] = temp;
+            }
+        }
+            
+            
+        }
+
+        System.out.println("\n ===== SELECT-SORTED ARRAY ===== (size: " + nums.length + ")");
+        for(int num: nums){
+            System.out.print(num + " ");
+        }
     }
 
     @Override
