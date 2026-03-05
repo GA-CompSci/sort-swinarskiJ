4cf2ed25d5a9e8a7dbc7338b0e7148fc77f45ad6
swinarskiJ <111909545+swinarskiJ@users.noreply.github.com>
2026-03-05 09:17:38 -0500

    sort4
    Co-authored-by: Dan Adiletta <dadiletta@users.noreply.github.com>

--- src/Activity1.java
+++ src/Activity1.java
@@ -1,17 +1,29 @@
 public class Activity1 extends IntegerManager implements PrintPretty{
     public static void main(String[] args) throws Exception {
         Activity1 thingie = new Activity1();
+        
+        //build
         thingie.buildList();
         thingie.printPretty();
+        
+        //shuffle
         thingie.shuffle();
+        thingie.printPretty();
+
+        //select sort
         thingie.selectionSort();
+        thingie.printPretty();
+        
+        thingie.selectionSort(true);
+        thingie.printPretty();
+
     }
 
     @Override
     void buildList() {
         nums = new int[20];
         for(int i = 0; i < nums.length; i++){
-            nums[i] = (int)(Math.random()*100);
+            nums[i] = (int)(Math.random()*121)-20;
         }
     }
 
@@ -35,11 +47,6 @@ public class Activity1 extends IntegerManager implements PrintPretty{
             nums[r] = temp;
         }
 
-        System.out.println("\n ===== SHUFFLED ARRAY ===== (size: " + nums.length + ")");
-        for(int num: nums){
-            System.out.print(num + " ");
-        }
-            
     }
 
     @Override
@@ -47,28 +54,49 @@ public class Activity1 extends IntegerManager implements PrintPretty{
        
     }
 
+    void selectionSort(boolean highToLow){
+        if(!highToLow){
+            selectionSort();
+        }
+        else{
+            for(int o = 0; o < nums.length-1; o++){
+
+            int largest = o;
+
+            for(int i = o+1; i < nums.length; i++){
+                if (nums[i]>nums[largest]) {
+                    // update the address of the smallest number 
+                    largest = i;  
+                }
+            }
+            // after I'm done confirming the smallest address, then I swap
+            int temp = nums[largest];
+            nums[largest] = nums[o];
+            nums[o] = temp;
+            
+        }
+        }
+    }
+
     @Override
     void selectionSort() {
-        int i,j,temp;
-        for (i = 0; i < nums.length; i++){
-            int smallest = i;
+        for(int o = 0; o < nums.length-1; o++){
 
-            for(j = i+1; j <nums.length; j++){
-                if(nums[j]<nums[smallest]){
-                    smallest = j;
-                    temp = nums[j];
-                    nums[j] = nums[i];
-                    nums[i] = temp;
+            int smallest = o;
+
+            for(int i = o+1; i < nums.length; i++){
+                if (nums[i]<nums[smallest]) {
+                    // update the address of the smallest number 
+                    smallest = i;  
+                }
             }
+            // after I'm done confirming the smallest address, then I swap
+            int temp = nums[smallest];
+            nums[smallest] = nums[o];
+            nums[o] = temp;
+            
         }
             
-            
-        }
-
-        System.out.println("\n ===== SELECT-SORTED ARRAY (NOT DONE) ===== (size: " + nums.length + ")");
-        for(int num: nums){
-            System.out.print(num + " ");
-        }
     }
 
     @Override
